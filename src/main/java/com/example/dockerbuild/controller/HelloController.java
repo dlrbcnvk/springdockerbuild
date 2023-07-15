b0d4c8768ef0c254856d52c049b001eb4f07f9c8
dlrbcnvk <dlrbcnvk@naver.com>
2023-07-15 22:50:40 +0900

    ec2에 자동배포 2

--- src/main/java/com/example/dockerbuild/controller/HelloController.java
+++ src/main/java/com/example/dockerbuild/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloController {
 
+    // 왜 ec2 에 자동배포가 안 되냐
     @GetMapping("/hello")
     public String hello() {
         log.info("GET /hello called");
