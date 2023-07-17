b01453bc5dc14c094ac027749531d5ddb484b4db
dlrbcnvk <dlrbcnvk@naver.com>
2023-07-17 16:34:14 +0900

    230717 16:34

--- src/main/java/com/example/dockerbuild/controller/HelloController.java
+++ src/main/java/com/example/dockerbuild/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HelloController {
-    
+
     @GetMapping("/hello")
     public String hello() {
         log.info("GET /hello called");
